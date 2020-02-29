656fe263d8c9c35210e2f2388c4d727c8143f51e
SakharovDN <SakharovDN@users.noreply.github.com>
2020-03-01 00:28:09 +0700

    feature(PhoneNumber): Создан класс PhoneNumber, добавлен класс Number для хранения номера телефона

---
+++ ContactsAppUI/ContactsAppUI/PhoneNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ContactsAppUI
+{
+    internal class PhoneNumber
+    {
+        private long _number;
+
+        public long Number
+        {
+            get { return _number; }
+
+            set
+            {
+                if (Convert.ToString(value).Length != 11)
+                    throw new Exception("Номер должен состоять из 11 цифр");
+                else if (value / 10000000000 != 7)
+                    throw new Exception("Номер должен начинаться с цифры 7");
+                else
+                    _number = value;
+            }
+        }
+    }
+}
