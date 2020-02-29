150d5a4f5d08abbfc4679f6b3c3c4c33af7d84b2
SakharovDN <SakharovDN@users.noreply.github.com>
2020-02-29 20:19:45 +0700

    feature(Contact):создан класс Contact, добавлено свойство Surname для хранения фамилии, Name для хранения имени, Birthday для хранения даты рождения, Email для хранения e-mail, IDVK для хранения idVK

---
+++ ContactsAppUI/ContactsAppUI/Contact.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Globalization;
+
+namespace ContactsAppUI
+{
+    public class Contact
+    {
+        private string _surname;
+
+        public string Surname
+        {
+            get { return _surname; }
+
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new Exception("Фамилия не должна превышать 50 символов");
+                }
+                else
+                    _surname = value;
+                TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+                _surname = ti.ToTitleCase(_surname);
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+                if (value.Length > 50)
+                {
+                    throw new Exception("Фамилия не должна превышать 50 символов");
+                }
+                else
+                    _name = value;
+                TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+                _name = ti.ToTitleCase(_name);
+            }
+        }
+
+        private DateTime _birthday = new DateTime();
+        
+        public DateTime Birthday
+        {
+            get { return _birthday; }
+            set
+            {
+                DateTime dateFrom = new DateTime(1900, 01, 01);
+                if (value > DateTime.Today && value < dateFrom)
+                    throw new Exception("Дата рождения не корректна");
+                else
+                    _birthday = value;
+
+            }
+        }
+
+        private string _email;
+
+        public string Email { get; set; }
+
+        private string _iDVK;
+
+        public string IDVK
+        {
+            get { return _iDVK; }
+
+            set
+            {
+                if (value.Length > 15)
+                {
+                    throw new Exception("Фамилия не должна превышать 50 символов");
+                }
+                else
+                    _iDVK = value;
+            }
+        }
+
+    }
+}
