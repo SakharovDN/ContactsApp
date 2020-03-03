9ca77696d7909da7eae12c821febff7820453030
SakharovDN <SakharovDN@users.noreply.github.com>
2020-03-03 15:41:29 +0700

    feature(Project): Добавлен класс Project, содержащий список всех контактов

---
+++ ContactsAppUI/ContactsAppUI/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsAppUI
+{
+    public class Project
+    {
+        private List<Contact> contacts;
+    }
+}
